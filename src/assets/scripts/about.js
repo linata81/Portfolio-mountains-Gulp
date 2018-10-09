73007f0b3a40cd33f23ed2485e95da58fb382662
Natalia Lysak <linata81@gmail.com>
2018-10-09 12:37:03 +0300

    adaptive

--- src/assets/scripts/about.js
+++ src/assets/scripts/about.js
@@ -1,1 +1,13 @@
 import "./modules/example";
+
+(function () {
+
+  var menuWrapper = document.querySelector('.hamburger-menu__wrapper-pic');
+  var hamburger = document.querySelector('.hamburger-pic');
+
+  menuWrapper.onclick = function () {
+    hamburger.classList.toggle('animate');
+  }
+
+})();
+
