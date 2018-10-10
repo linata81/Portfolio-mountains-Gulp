4f5ad195d49691c147686d6f7cc5c1f8c53868b6
Natalia Lysak <linata81@gmail.com>
2018-10-11 00:41:47 +0300

    blur

--- src/assets/scripts/works.js
+++ src/assets/scripts/works.js
@@ -1,3 +1,26 @@
 import "./modules/hero";
 
+var blur = (function(){
+  var wrapper = document.querySelector('.modal-form__wrapper'),
+      form = document.querySelector('.modal-form__bg');
+
+      return {
+        set(){
+          var imgWidth = document.querySelector('.reviews__bg').offsetWidth,
+              posLeft = -wrapper.offsetLeft,
+              posTop = -wrapper.offsetTop,
+              blurCSS = form.style;
+
+          blurCSS.backgroundSize = `${imgWidth}px auto`;
+          blurCSS.backgroundPosition = `${posLeft}px ${posTop}px`;
+
+        }
+      }
+}());
+
+blur.set();
+window.onresize = function(){
+  blur.set();
+}
+
 
