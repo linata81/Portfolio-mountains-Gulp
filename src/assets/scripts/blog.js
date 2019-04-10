8fcf360fad964c39fe213234600b3ace66d4992a
linata81 <linata81@gmail.com>
2019-04-10 12:08:38 +0300

    adaptive

--- src/assets/scripts/blog.js
+++ src/assets/scripts/blog.js
@@ -5,15 +5,35 @@ import "./modules/hamburgerMenu";
 import "./modules/asideMenu";
 import "./modules/btnScrollDown";
 
+var menu = document.querySelector('.aside-menu');
+var semicircle = document.querySelector('.semicircle');
+
 (function asideMenuOpen(){
-  var semicircle = document.querySelector('.semicircle');
-  var menu = document.querySelector('.aside-menu');
+  // var semicircle = document.querySelector('.semicircle');
+  // var menu = document.querySelector('.aside-menu');
 
   semicircle.addEventListener('click', function(){
     menu.classList.toggle('js-aside-open');
   })
 })();
 
+(function asideMenuClose(){
+  function closePanel(){
+    document.addEventListener('click', function(e){
+      menu.classList.remove('js-aside-open');
+      e.stopPropagation();
+    });
+  }
+  if (document.body.clientWidth<480) {
+    document.addEventListener('DOMContentLoaded', closePanel);
+    semicircle.addEventListener('click', function(e){
+      e.stopPropagation();
+    });
+    menu.addEventListener('click', function(e){
+      e.stopPropagation();
+    })
+  }
+})();
 
 
 
